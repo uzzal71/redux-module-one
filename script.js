156a1efa8347a835f7079e78ba7224cff89b1be0
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-06 12:21:14 +0600

    Rewrite the counter application

--- script.js
+++ script.js
@@ -4,15 +4,49 @@ const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
 // initial state
-let count = 0;
+const initialState = {
+    value: 0,
+};
 
-// event listeners
-incrementEl.addEventListener('click', () => {
-    count++;
-    counterEl.innerText = count;
+// create reducer function
+function counterReducer(state = initialState, action) {
+    if (action.type === "increment") {
+        return {
+            ...state,
+            value: state.value + 1,
+        };
+    } else if (action.type === "decrement") {
+        return {
+            ...state,
+            value: state.value - 1,
+        };
+    } else {
+        return state;
+    }
+}
+
+// create store
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+    const state = store.getState();
+    counterEl.innerText = state.value.toString();
+};
+
+// update UI initially
+render();
+
+store.subscribe(render);
+
+// button click listeners
+incrementEl.addEventListener("click", () => {
+    store.dispatch({
+        type: "increment",
+    });
 });
 
-decrementEl.addEventListener('click', () => {
-    count--;
-    counterEl.innerText = count;
+decrementEl.addEventListener("click", () => {
+    store.dispatch({
+        type: "decrement",
+    });
 });
